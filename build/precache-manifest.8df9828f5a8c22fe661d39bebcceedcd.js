0bd352a569273f91182d852a42f5bb25dfcf5373
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-04-15 12:55:28 -0700

    frontEnd

---
+++ build/precache-manifest.8df9828f5a8c22fe661d39bebcceedcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0230e32e9cd7639bcd6e175d8fef59ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c58f85229dfdc6e66231",
+    "url": "/static/css/main.73334574.chunk.css"
+  },
+  {
+    "revision": "de2f2bed8cda2e046ad7",
+    "url": "/static/js/2.9fea7bc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9fea7bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58f85229dfdc6e66231",
+    "url": "/static/js/main.88518a23.chunk.js"
+  },
+  {
+    "revision": "d97250d2585b20f244a7",
+    "url": "/static/js/runtime-main.449dc349.js"
+  }
+]);
--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
+app.use(express.static('build'))
 app.use(cors());
 app.use(express.json())
-
 app.use(express.urlencoded({extended:false}));
 
 let notes = [
